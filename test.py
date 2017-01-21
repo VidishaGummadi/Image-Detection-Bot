dd44421ffc1b3f8fb1440697cdaee58f7985bdaf
cmrmahesh <cmr.mahesh@gmail.com>
2017-01-21 17:16:49 +0530

    bye bye indentation

--- test.py
+++ test.py
@@ -12,62 +12,62 @@ def download_image(url):
     del response
 
 def detect_labels(url):
-	vision_client = vision.Client()
-	# image = client.image(filename='test.jpg')
-	download_image(url)
+    vision_client = vision.Client()
+    # image = client.image(filename='test.jpg')
+    download_image(url)
 
-	file_name = os.path.join(
-	        os.path.dirname(__file__),
-	        'test.jpg')
-
-
-	with io.open(file_name, 'rb') as image_file:
-		content = image_file.read()
-		image = vision_client.image(content=content)
-
-	# Performs label detection on the image file
-	
-	labels = image.detect_labels()
-	slabels =["Labels:"]
-	for label in labels:
-		slabels.append(label.description)
+    file_name = os.path.join(
+            os.path.dirname(__file__),
+            'test.jpg')
 
 
-	# Performs Face detection on the image file
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+        image = vision_client.image(content=content)
 
-	faces = image.detect_faces()
-	facelist = ["\nFaces:"]
-	if not faces:
-		facelist.append("Sorry!! No faces recognized")
-	else:
-		for face in faces:
-			facelist.append("anger-"+face.emotions.anger.value)
-			facelist.append("joy-"+face.emotions.joy.value)
-			facelist.append("surprise-"+face.emotions.surprise.value)
-			facelist.append("sorrow-"+face.emotions.sorrow.value)
-
-	# Performs Text detection on the image file
-	texts = image.detect_text()
-	textlist = ["\nTexts:"]
-	if not texts:
-		textlist.append("Sorry!! No Text Found")
-	else:
-		for text in texts:
-			textlist.append(text.description)
+    # Performs label detection on the image file
+    
+    labels = image.detect_labels()
+    slabels =["Labels:"]
+    for label in labels:
+        slabels.append(label.description)
 
 
-	return '\n'.join(slabels+facelist+textlist)
+    # Performs Face detection on the image file
+
+    faces = image.detect_faces()
+    facelist = ["\nFaces:"]
+    if not faces:
+        facelist.append("Sorry!! No faces recognized")
+    else:
+        for face in faces:
+            facelist.append("anger-"+face.emotions.anger.value)
+            facelist.append("joy-"+face.emotions.joy.value)
+            facelist.append("surprise-"+face.emotions.surprise.value)
+            facelist.append("sorrow-"+face.emotions.sorrow.value)
+
+    # Performs Text detection on the image file
+    texts = image.detect_text()
+    textlist = ["\nTexts:"]
+    if not texts:
+        textlist.append("Sorry!! No Text Found")
+    else:
+        for text in texts:
+            textlist.append(text.description)
+
+
+    return '\n'.join(slabels+facelist+textlist)
 
 
 # def detect_labels_from_url(uri):
-# 	"""Detects labels in the file located in Google Cloud Storage."""
-# 	vision_client = vision.Client()
-# 	image = vision_client.image(source_uri=uri)
-# 	labels = image.detect_labels()
-# 	print('Labels:')
-# 	shots =[]
-# 	for label in labels:
-# 		shots.append(label)
-# 	return shots
+#   """Detects labels in the file located in Google Cloud Storage."""
+#   vision_client = vision.Client()
+#   image = vision_client.image(source_uri=uri)
+#   labels = image.detect_labels()
+#   print('Labels:')
+#   shots =[]
+#   for label in labels:
+#       shots.append(label)
+#   return shots
     
 # print detect_labels()
